3ba2fbbfab7feea95d6d1b40996daad3d65c4005
Mark <mark.kapkan@gmail.com>
2024-04-02 22:23:09 +0300

    Wrote ending of css and made Javascript scor ecounter scheme

---
+++ script.js
@@ -0,0 +1,41 @@
+//Getting data by transferring tags in variables//
+let playerOne = document.getElementById('playerOne')
+let playerTwo = document.getElementById('playerTwo')
+let scoreOne = document.getElementById('scoreOne')
+let scoreTwo = document.getElementById('scoreTwo')
+let dice = document.getElementById('dice')
+let giveUp = document.getElementById('giveUp')
+let opponentsTurn = document.getElementById('nextTime')
+let endGame = document.getElementById('stopGame')
+//Making turns (you and Opponent)//
+let Turn = 1
+opponentsTurn.addEventListener('click', function(){
+    if (Turn == 1){
+        Turn++
+    }else{
+        Turn--
+    }
+})
+//Making score script//
+let scoreOnen = 0
+let scoreTwon = 0
+let AddictionOne = 0
+let AddictionTwo = 0
+playerOne.addEventListener('click' , function() {
+    if (Turn == 1){
+    AddictionOne = Math.floor(Math.random()*6)
+    for(let i=0; i<=AddictionOne; i++){
+        scoreOnen++
+    }
+    scoreOne.textContent = `Score = ${scoreOnen}`
+    }
+})
+playerTwo.addEventListener('click' , function() {
+    if (Turn == 2){
+    AddictionTwo = Math.floor(Math.random()*6)
+    for(let itwo=0; itwo<=AddictionTwo; itwo++){
+        scoreTwon++
+    }
+    scoreTwo.textContent = `Score = ${scoreTwon}`
+    }
+})
